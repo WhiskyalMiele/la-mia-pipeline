40ca7df24ae34b80658f239b5c52e08512ed1706
WhiskyalMiele <ingrassiasara0@gmail.com>
2026-07-17 10:42:30 +0200

    Pipeline con costanzo

---
+++ test/sum.test.js
@@ -0,0 +1,8 @@
+// test/sum.test.js
+const test = require('node:test');
+const assert = require('node:assert');
+const { sum } = require('../src/sum');
+
+test('1 + 2 = 3', () => {
+  assert.strictEqual(sum(1, 2), 3);
+});
